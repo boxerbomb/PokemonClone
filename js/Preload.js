60f27d5afeb3d663ec0658f61bbc19a57aa8f98d
Pablo Farias Navarro <fariazz@gmail.com>
2014-08-28 15:53:40 +1000

    polished

--- js/Preload.js
+++ js/Preload.js
@@ -5,22 +5,22 @@ TopDownGame.Preload = function(){};
 
 TopDownGame.Preload.prototype = {
   preload: function() {
-  	//show loading screen
-    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloadbar');
+    //show loading screen
+    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadbar');
     this.preloadBar.anchor.setTo(0.5);
 
     this.load.setPreloadSprite(this.preloadBar);
 
-  	//load game assets
+    //load game assets
     this.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('gameTiles', 'assets/images/tiles.png');
     this.load.image('greencup', 'assets/images/greencup.png');
     this.load.image('bluecup', 'assets/images/bluecup.png');
     this.load.image('player', 'assets/images/player.png');
-  	this.load.image('browndoor', 'assets/images/browndoor.png');
-  	
+    this.load.image('browndoor', 'assets/images/browndoor.png');
+    
   },
   create: function() {
-  	this.state.start('Game');
+    this.state.start('Game');
   }
 };
