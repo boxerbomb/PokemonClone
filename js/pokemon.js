998bfe85a7ab3054c7ba3aa733af4d039bd08484
Dallaire <jacobdallaire@yahoo.ca>
2017-06-07 22:34:23 -0400

    added in Pokemon type attributes and used _ in front of attribute names to conform to javascript style for private values.

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,71 +1,73 @@
 // species stats are an array of base stats
-//[number, name, hp, atk, def, spAtk,spDef,speed]
+//[number,nickname, hp, atk, def, spAtk,spDef,speed,type1,type2]
 function Pokemon(species, lvl){
   //Base attributes
-  this.number=species[0];
-  this.nickname=species[1];  //can be updated with nickname on capture
-  this.baseHP=species[2];
-  this.baseAtk=species[3];
-  this.baseDef=species[4];
-  this.baseSpAtk=species[5];
-  this.baseSpDef=species[6];
-  this.baseSpeed=species[7];
+  this._number=species[0];
+  this._nickname=species[1];  //can be updated with nickname on capture
+  this._baseHP=species[2];
+  this._baseAtk=species[3];
+  this._baseDef=species[4];
+  this._baseSpAtk=species[5];
+  this._baseSpDef=species[6];
+  this._baseSpeed=species[7];
+  this._type1=species[8];
+  this._type2=species[9];
 
   //IVs
-  this.hpIV=Math.floor(Math.random() * 32);
-  this.atkIV=Math.floor(Math.random() * 32);
-  this.defIV=Math.floor(Math.random() * 32);
-  this.spAtkIV=Math.floor(Math.random() * 32);
-  this.spDefIV=Math.floor(Math.random() * 32);
-  this.speedIV=Math.floor(Math.random() * 32);
+  this._hpIV=Math.floor(Math.random() * 32);
+  this._atkIV=Math.floor(Math.random() * 32);
+  this._defIV=Math.floor(Math.random() * 32);
+  this._spAtkIV=Math.floor(Math.random() * 32);
+  this._spDefIV=Math.floor(Math.random() * 32);
+  this._speedIV=Math.floor(Math.random() * 32);
 
   //EVs
-  this.totalEV=0;
-  this.hpEV=0;
-  this.atkEV=0;
-  this.defEV=0;
-  this.spAtkEV=0;
-  this.spDefEV=0;
-  this.speedEV=0;
+  this._totalEV=0;
+  this._hpEV=0;
+  this._atkEV=0;
+  this._defEV=0;
+  this._spAtkEV=0;
+  this._spDefEV=0;
+  this._speedEV=0;
 
   //assigns stats based on formula
-  this.maxhp=((baseHP * 2 + hpIV + hpEV/4) * lvl/100) + 10 + lvl;
-  this.atk=((baseAtk * 2 + atkIV + atkEV/4) * lvl/100) + 5;
-  this.def=((baseDef * 2 + defIV + defEV/4) * lvl/100) + 5;
-  this.spAtk=((baseSpAtk * 2 + spAtkIV + spAtkEV/4) * lvl/100) + 5;
-  this.spDef=((baseSpDef * 2 + spDefIV + spDefEV/4) * lvl/100) + 5;
-  this.speed=((baseSpeed * 2 + speedIV + speedEV/4) * lvl/100) + 5;
+  this._maxhp=((_baseHP * 2 + _hpIV + _hpEV/4) * lvl/100) + 10 + lvl;
+  this._atk=((_baseAtk * 2 + _atkIV + _atkEV/4) * lvl/100) + 5;
+  this._def=((_baseDef * 2 + _defIV + _defEV/4) * lvl/100) + 5;
+  this._spAtk=((_baseSpAtk * 2 + _spAtkIV + _spAtkEV/4) * lvl/100) + 5;
+  this._spDef=((_baseSpDef * 2 + _spDefIV + _spDefEV/4) * lvl/100) + 5;
+  this._speed=((_baseSpeed * 2 + _speedIV + _speedEV/4) * lvl/100) + 5;
 
 
-  this.currenthp=this.maxhp;
-  this.statusEffect=null;
+  this._currenthp=this._maxhp;
+  this._statusEffect=null;
 };
 
 // getters
 Pokemon.prototype.getNumber = function () {
-  return this.number;
+  return this._number;
 };
 Pokemon.prototype.getName = function () {
-  return this.nickname;
+  return this._nickname;
 };
 Pokemon.prototype.getMaxHP = function () {
-  return this.maxhp;
+  return this._maxhp;
 };
 Pokemon.prototype.getAtk = function () {
-  return this.atk;
+  return this._atk;
 };
 Pokemon.prototype.getDef = function () {
-  return this.def;
+  return this._def;
 };
 Pokemon.prototype.getSpAtk = function () {
-  return this.spAtk;
+  return this._spAtk;
 };
 Pokemon.prototype.getSpDef = function () {
-  return this.spDef;
+  return this._spDef;
 };
 Pokemon.prototype.getSpeed = function () {
-  return this.speed;
+  return this._speed;
 };
 Pokemon.prototype.getCurrenthp = function () {
-  return currenthp;
+  return this._currenthp;
 };
